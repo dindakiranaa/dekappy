2d5f187191b6dc7744ecb6158e44e188e194b066
dindakiranaa <dindakirana104@gmail.com>
2023-09-12 17:20:59 +0700

    update

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class mainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('/main/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('/main/')
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_show_main(self):
+        response = Client().get('/main/')
+        self.assertEqual(response.context['appname'], 'DEKAPPY')
+        self.assertEqual(response.context['name'], 'Dinda Kirana Khairunnisa')
+        self.assertEqual(response.context['class'], 'PBP C')
+
